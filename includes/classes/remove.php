9c059ccee6983be9e40e91341fd9e2d500dbfb01
dnnsjsk <dnnsjsk@gmail.com>
2020-09-28 18:58:22 +0200

    Add new remove functions

--- includes/classes/remove.php
+++ includes/classes/remove.php
@@ -61,6 +61,7 @@ class remove {
 		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		add_filter('emoji_svg_url', '__return_false');
 		add_filter( 'tiny_mce_plugins', function ( $plugins ) {
 			if ( is_array( $plugins ) ) {
 				return array_diff( $plugins, array( 'wpemoji' ) );
@@ -78,6 +79,71 @@ class remove {
 			return $urls;
 		}, 10, 2 );
 
+	}
+
+	/**
+	 * Remove Gutenberg blocks.
+	 *
+	 * @since 0.01
+	 */
+
+	public static function gutenberg()
+	{
+		add_action('wp_enqueue_scripts', function () {
+			wp_dequeue_style('wp-block-library');
+		}, 200);
+	}
+
+	/**
+	 * Remove extra RSS feeds.
+	 *
+	 * @since 0.01
+	 */
+
+	public static function extraRss()
+	{
+		add_filter('feed_links_show_comments_feed', '__return_false');
+	}
+
+	/**
+	 * Remove recent comments CSS.
+	 *
+	 * @since 0.01
+	 */
+
+	public static function recentCommentsCss()
+	{
+		add_filter('show_recent_comments_widget_style', '__return_false');
+	}
+
+	/**
+	 * Remove gallery CSS.
+	 *
+	 * @since 0.01
+	 */
+
+	public static function galleryCss()
+	{
+		add_filter('use_default_gallery_style', '__return_false');
+	}
+
+	/**
+	 * Remove WordPress.
+	 *
+	 * @since 0.01
+	 */
+
+	public static function wordpress()
+	{
+		add_filter('the_generator', '__return_false');
+		remove_action('wp_head', 'rsd_link');
+		remove_action('wp_head', 'wlwmanifest_link');
+		remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);
+		remove_action('wp_head', 'wp_generator');
+		remove_action('wp_head', 'wp_shortlink_wp_head', 10);
+		remove_action('wp_head', 'rest_output_link_wp_head', 10);
+		remove_action('wp_head', 'wp_oembed_add_discovery_links');
+		remove_action('wp_head', 'wp_oembed_add_host_js');
 	}
 
 }
